038bb7ca29ab66c19207d9521fdc536df3750c7d
PedroMolon <pedroh.molonr@gmail.com>
2025-11-25 17:52:45 -0300

    feat: criação de migration

---
+++ src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE stadiums (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    capacity INTEGER,
+    url_img VARCHAR(255)
+);
+
+CREATE TABLE clubs (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    founded DATE NOT NULL,
+    url_img VARCHAR(255),
+    stadium_id INTEGER,
+    CONSTRAINT fk_stadium_club FOREIGN KEY (stadium_id) REFERENCES stadiums(id)
+);
+
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    position VARCHAR(255),
+    shirt_number INTEGER,
+    url_img VARCHAR(255),
+    club_id INTEGER,
+    CONSTRAINT fk_club_player FOREIGN KEY (club_id) REFERENCES clubs(id)
+);
