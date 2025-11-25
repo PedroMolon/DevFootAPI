905555bafef3c32f0309461105eed06f4a125799
PedroMolon <pedroh.molonr@gmail.com>
2025-11-25 17:41:40 -0300

    first commit

---
+++ src/main/java/com/pedromolon/DevFootAPI/DevFootApiApplication.java
@@ -0,0 +1,13 @@
+package com.pedromolon.DevFootAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevFootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedromolon/DevFootAPI/DevFootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromolon.DevFootAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevFootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
